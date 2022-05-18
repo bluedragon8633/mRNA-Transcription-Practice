c75eea85ff47e717191a6d38792a606b6fe385ca
bluedragon8633 <94876427+bluedragon8633@users.noreply.github.com>
2022-05-18 13:22:57 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+
+isRNA = True
+score = 0
+original = ['T','C','A','G',' ']
+flipped = ['A','G','T','C',' ']
+sequence = ''
+answer = ''
+
+print('Transcribe to mRNA!')
+if isRNA:
+    flipped[2] = 'U'
+for a in range(5):
+    sequence = ''
+    answer = ''
+    for b in range(4):
+        for c in range(3):
+            sequence = sequence + original[random.randint(0,3)]
+        if b < 3:
+            sequence = sequence + ' '
+    for d in range(len(sequence)):
+        answer = answer + flipped[original.index(sequence[d])]
+    print(sequence)
+    userIn = input()
+    if userIn == answer:
+        score += 1
+        print('Correct')
+    else:
+        print('Incorrect. Correct answer: ' + answer)
+        if "T" in userIn:
+            print('Remember, mRNA doesn\'t have T\'s!')
+print('Your final score is: ' + score)
